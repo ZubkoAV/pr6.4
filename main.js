9178d1e1136a18194f81b804badd21fe0f17ae52
ZubkoAV <zubkoandriy2005@gmail.com>
2024-05-30 10:48:17 +0300

    commit

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
     document.getElementById('regForm').addEventListener('submit', function(event) {
         event.preventDefault();
-
-        if (username && password) {
             window.location.href = '2.html';
-        } else {
-            const messageDiv = document.getElementById('message');
-            messageDiv.innerHTML = 'Будь ласка, заповніть усі поля!';
-        }
     });
